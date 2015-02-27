d16a14e9e2732af551598d6fd2e51d3260e6de58
wscete <wcsete@gmail.com>
2015-02-26 22:30:37 -0800

    added build

---
+++ dist/js/main.js
@@ -0,0 +1,25 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+// var $        = require('../../../lib/jquery/jquery');
+// var Uploader = require('./Uploader');
+
+var Main = {
+
+	initialize : function() {
+
+		'use strict';
+
+		// Elements
+		// this.$uploaderElement = $('#uploader-element');
+
+		// Make new uploader
+		// var loader = new Uploader( this.$uploaderElement );
+		console.log('initialized!');
+
+	}
+
+};
+
+Main.initialize();
+
+},{}]},{},[1]);
---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+
+// var $        = require('../../../lib/jquery/jquery');
+// var Uploader = require('./Uploader');
+
+var Main = {
+
+	initialize : function() {
+
+		'use strict';
+
+		// Elements
+		// this.$uploaderElement = $('#uploader-element');
+
+		// Make new uploader
+		// var loader = new Uploader( this.$uploaderElement );
+		console.log('initialized!');
+
+	}
+
+};
+
+Main.initialize();
